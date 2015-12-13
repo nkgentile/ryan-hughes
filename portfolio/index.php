21383cf60e00696fc2c601c76ae9f093eaca6468
Noah Gentile <noah.gentile@gmail.com>
2015-12-13 15:04:15 -0800

    Mechanics in place

--- portfolio/index.php
+++ portfolio/index.php
@@ -1,5 +1,10 @@
 <html>
 <head>
+	<title>Ryan James Hughes Portfolio</title>
+	<script src="assets/js/rh.js" type="text/javascript"></script>
+	<style>
+		@import "assets/css/body.css";
+	</style>
 </head>
 <body>
 </body>
